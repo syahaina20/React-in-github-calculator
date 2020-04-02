832917aa01b1515c31fcc55896f3c861cd2f6256
syahaina20 <syahaina20@gmail.com>
2020-04-02 14:18:22 +0800

    Updates

---
+++ precache-manifest.45fbe9949ba8a23b1e09cb0836738ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e17031603dee7d8323c5a533289be71",
+    "url": "/React-in-github-calculator/index.html"
+  },
+  {
+    "revision": "f88ba38d021ae01e4453",
+    "url": "/React-in-github-calculator/static/css/main.23f0e687.chunk.css"
+  },
+  {
+    "revision": "a1980570b5c1ade4f03a",
+    "url": "/React-in-github-calculator/static/js/2.33b83415.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-in-github-calculator/static/js/2.33b83415.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f88ba38d021ae01e4453",
+    "url": "/React-in-github-calculator/static/js/main.cb32ed64.chunk.js"
+  },
+  {
+    "revision": "7020b72e3114f60b8594",
+    "url": "/React-in-github-calculator/static/js/runtime-main.81cb2bac.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-in-github-calculator/precache-manifest.45fbe9949ba8a23b1e09cb0836738ce6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-in-github-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
